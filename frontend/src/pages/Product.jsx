d2e19f4aa37f3b5243a2ddfdd2afe05a6a8868c4
ahmet <ahmtyln.06@gmail.com>
2024-10-04 01:26:20 +0200

    size logic

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,14 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products,currency} = useContext(ShopContext);
+  const {products,currency, addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
+  const [size, setSize] = useState('')
 
   const fetchProductData = async() =>{
     products.map((item) =>{
@@ -61,12 +63,36 @@ const Product = () => {
               <p>Select Size</p>
               <div className="flex gap-2">
                 {productData.sizes.map((item,index) => (
-                  <button key={index}>{item}</button>
+                  <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
                 ))}
               </div>
             </div>
+            <button onClick={() => addToCart(productData.id, size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+            <hr className="mt-8 sm:w-4/5" />
+            <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+              <p>100% Original product.</p>
+              <p>Cash on delivery is available on this product.</p>
+              <p>Easy return and exchange policy within 7 days.</p>
+            </div>
           </div>
       </div>
+      {/* ------------ Description & Review Section -------------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Commodi et, reprehenderit, consectetur consequuntur libero sunt quibusdam amet fugit dolor 
+            incidunt harum enim hic ad veritatis sapiente deserunt maiores! Id, illo.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque, alias.</p>
+        </div>
+      </div>
+
+      {/* ------------ Display related products -------------- */}
+
+      <RelatedProducts category = {productData.category} subCategory = {productData.subCategory}  />
     </div>
   ) : <div className="opacity-0"></div>
 }
